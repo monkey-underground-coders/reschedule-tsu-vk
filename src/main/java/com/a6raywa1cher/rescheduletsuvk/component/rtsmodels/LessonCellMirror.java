e294ab5bb2f236c99ad06b79c9a7e9c2cdb9bc33
6rayWa1cher <info@a6raywa1cher.com>
2020-02-17 18:08:19 +0300

    fix

--- src/main/java/com/a6raywa1cher/rescheduletsuvk/component/rtsmodels/LessonCellMirror.java
+++ src/main/java/com/a6raywa1cher/rescheduletsuvk/component/rtsmodels/LessonCellMirror.java
@@ -1,12 +1,14 @@
 package com.a6raywa1cher.rescheduletsuvk.component.rtsmodels;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LessonCellMirror {
 	private WeekSign weekSign;
 
